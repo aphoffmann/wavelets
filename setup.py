97fdf11d929465d79987d9698d8e93a273b7354d
Alex Hoffmann <alexpaulhoffmann@gmail.com>
2023-09-14 11:14:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 config = {
-        'description': 'Continuous wavelet analysis in Python',
-        'author': "Aaron O'Leary",
-        'url': 'http://github.com/aaren/wavelets',
-        'author_email': 'eeaol@leeds.ac.uk',
-        'version': '0.1',
-        'packages': ['wavelets'],
-        'name': 'wavelets',
-        }
+    'description': 'Continuous wavelet analysis in Python',
+    'author': "Aaron O'Leary",
+    'url': 'http://github.com/aaren/wavelets',
+    'author_email': 'eeaol@leeds.ac.uk',
+    'version': '0.1',
+    'packages': ['wavelets'],
+    'name': 'wavelets',
+    'setup_requires': ['numpy'],
+    'install_requires': ['numpy', 'scipy']
+}
 
 setup(**config)
